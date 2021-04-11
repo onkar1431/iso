2df65199a573435e06b13aa648ae3be4cd06fb6a
unknown <nagarkaronkar@gmail.com>
2021-04-11 15:09:26 +0530

    Added Login Modal component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import AppBar from "@material-ui/core/AppBar";
+import Badge from "@material-ui/core/Badge";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -6,34 +7,56 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Dialog from "@material-ui/core/Dialog";
 import Grid from "@material-ui/core/Grid";
-import IconButton from '@material-ui/core/IconButton';
-import InputBase from '@material-ui/core/InputBase';
-import Paper from '@material-ui/core/Paper';
+import IconButton from "@material-ui/core/IconButton";
+import InputBase from "@material-ui/core/InputBase";
+import Paper from "@material-ui/core/Paper";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from "@material-ui/icons/Menu";
 import CameraIcon from "@material-ui/icons/PhotoCamera";
-import SearchIcon from '@material-ui/icons/Search';
-import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import React, { useEffect, useState } from "react";
+import Login from "./components/Login/Login";
 import useStyles from "./data/config";
 import courses from "./data/mock-data";
 
 export default function Album() {
   const classes = useStyles();
-  const [courseList, setCourseList] = React.useState(courses[0].lessons);
-  const [filteredCourses, setFilteredCourses] = React.useState([]);
+  const [courseList, setCourseList] = useState(courses[0].lessons);
+  const [filteredCourses, setFilteredCourses] = useState([]);
+  const [isOpenModal, setIsOpenModal] = useState(false);
   const filterCourses = (event) => {
-    setFilteredCourses(courseList.filter((item) => item.name === event.target.value));
-  }
+    setFilteredCourses(
+      courseList.filter((item) => item.name === event.target.value)
+    );
+  };
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (filteredCourses.length > 0) {
       setCourseList(filteredCourses);
     } else {
       setCourseList(courses[0].lessons);
     }
-  }, [filteredCourses])
+  }, [filteredCourses]);
+
+  const openLoginModal = () => {
+    setIsOpenModal(true);
+  };
+
+  const handleClose = () => {
+    setIsOpenModal(false);
+  };
+
+  const renderLoginModal = () => {
+    return (
+      <Dialog open={isOpenModal} onClose={handleClose}>
+        <Login />
+      </Dialog>
+    );
+  };
 
   return (
     <>
@@ -44,10 +67,17 @@ export default function Album() {
           <Typography variant="h6" color="inherit" noWrap>
             Dentsu
           </Typography>
+          <div className={classes.cart}>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
       <main>
-        <div className={classes.heroContent}>
+        <div className={classes.content}>
           <Container maxWidth="sm">
             <Typography
               component="h1"
@@ -65,7 +95,7 @@ export default function Album() {
               <InputBase
                 className={classes.input}
                 placeholder="Search Courses"
-                inputProps={{ 'aria-label': 'search courses' }}
+                inputProps={{ "aria-label": "search courses" }}
                 onChange={filterCourses}
               />
               <SearchIcon />
@@ -90,7 +120,11 @@ export default function Album() {
                     <div className={classes.authorName}>- {card.author}</div>
                   </CardContent>
                   <CardActions>
-                    <Button size="small" color="primary">
+                    <Button
+                      size="small"
+                      color="primary"
+                      onClick={openLoginModal}
+                    >
                       Add to Cart
                     </Button>
                   </CardActions>
@@ -114,6 +148,7 @@ export default function Album() {
           Something here to give the footer a purpose!
         </Typography>
       </footer>
+      {isOpenModal === true ? renderLoginModal() : null}
       {/* End footer */}
     </>
   );
